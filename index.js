1db54fc83cbecb8622567981f61f780ef25dbed1
MahmoudRumaneh <mahmoudrmn@gmail.com>
2023-10-12 19:21:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-function reverseString(word) {
-  return word.split("").reverse().join("");
+function drawHeart(radius) {
+  let heart = '';
+  for (let y = -radius; y <= radius; y++) {
+    let row = '';
+    for (let x = -radius; x <= radius; x++) {
+      let x2 = x / radius;
+      let y2 = y / radius;
+      let d = x2 * x2 + y2 * y2;
+      if (d <= 1) {
+        row += '❤️'; // Heart symbol
+      } else {
+        row += ' ';
+      }
+    }
+    heart += row + '\n';
+  }
+  console.clear(); // Clear the console for animation effect
+  console.log(heart);
 }
 
-function isPalindrome(word) {
-  const reversedWord = reverseString(word);
-  return word === reversedWord;
+const maxRadius = 15;
+let radius = 0;
+
+function animateHeartDrawing() {
+  if (radius <= maxRadius) {
+    drawHeart(radius);
+    radius++;
+    setTimeout(animateHeartDrawing, 100); // Adjust the animation speed
+  }
 }
 
-if (require.main === module) {
-  console.log("=>", isPalindrome("racecar"));
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+animateHeartDrawing();
